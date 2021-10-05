7e85cc247de3e2332a99c95b50b1a64bdce2a9f4
Sabrina Birabwa <89991227+Bira274@users.noreply.github.com>
2021-10-04 19:58:13 -0700

    Add javascript file

---
+++ js/intro.js
@@ -0,0 +1,29 @@
+// Multiple calls
+function showData(name, age) {
+    console.log(name, age)
+}
+showData ('Leah', 20)
+showData('Florence', 50)
+showData('Dan', 10)
+showData('Rachel', 30)
+showData('Francis', 40)
+
+// Parameter function
+function calculateCost(speed, time) {
+    let fare=5
+    console.log(cost= speed*time*fare)
+}
+function displayCost() {
+    console.log('Please pay UGX',calculateCost(speed, time))
+}
+calculateCost(50, 10)
+
+// Arrow function
+let  x=10
+let y=2
+divide = (x,y) => x/y; console.log(x,'divided by',y,'is equal to',divide(x,y),)
+
+// Loop
+for (let a=1; a<20; a++) {
+    console.log('I love Uganda',a)
+}
